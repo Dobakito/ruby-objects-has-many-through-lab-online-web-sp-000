636fdd3a7d3d83797cbc6509009e91350944bf6d
Michael Dobak <mdobak44@icloud.com>
2021-03-02 22:16:59 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,19 @@
+class Patient
+  attr_accessor :name, :doctor, :appointment
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def 
+  
+  def new_appointment(date, doctor)
+    Appointment.new(date, doctor, self)
+  end 
+  
+end 
