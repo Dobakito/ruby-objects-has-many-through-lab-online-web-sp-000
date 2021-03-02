34f13479e5deef3efc14a69ffc78bb479f168b88
Michael Dobak <mdobak44@icloud.com>
2021-03-02 22:26:22 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -11,6 +11,9 @@ class Patient
   end 
   
   def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end 
   end
   
   def new_appointment(date, doctor)
